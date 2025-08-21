b96907328166fea7d8cecb3452ad4a9fb9399914
Savva Melnikov <msavva@bk.ru>
2025-08-21 13:54:41 +0200

    init 3

--- 5.php
+++ 5.php
@@ -1,12 +1,31 @@
 <?
-class Entity
-{
-	public function copy(): static
-	{
-		$copiedEntity = new static();
-		// do something
-		return $copiedEntity;
-	}
+class PaymentProcessor {
+    private $paymentSystem;
 
-	//...
+    public function __construct(SwiftPaymentSystem $paymentSystem) {
+        $this->paymentSystem = $paymentSystem;
+    }
+
+    public function processPayment($amount) {
+        $this->paymentSystem->pay($amount);
+    }
 }
+
+
+// SWIFT.API.LIB
+class SwiftPaymentSystem {
+    public function pay($amount) {
+        echo "Paying $amount using old payment system.\n";
+    }
+}
+
+// MIR.API.LIB
+class MirPaymentSystem {
+    public function makePayment($amount) {
+        echo "Paying $amount using new payment system.\n";
+    }
+}
+
+// $mirPaymentSystem = new MirPaymentSystem();
+// $paymentProcessor = new PaymentProcessor($mirPaymentSystem);
+// $paymentProcessor->processPayment(100);
